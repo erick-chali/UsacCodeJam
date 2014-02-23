5e34975860dae00e94180b7e6e6f73059d53f10d
Erick Chali <erickdemon@gmail.com>
2014-02-22 19:40:22 -0600

    Cambie el nombre de la clase.
    Lo había echo con otro nombre y no me compilaba, pero si lo hice yo.

--- AdivinaNumero.java
+++ AdivinaNumero.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 
-public class CodeJam {
+public class AdivinaNumero {
 	public static int adivina;
 	public static void main(String[] args) {
 		System.out.println("--------Adivina El Numero--------");
